e39b6d2aa5cb33124a334edbf8885690666c009b
Hitesh Agrawal <hitesha@Hiteshs-MacBook-Air.local>
2015-10-29 20:19:16 +0800

    Adding more beatiful soup scripts

--- networking/beautifulsoup/href.py
+++ networking/beautifulsoup/href.py
@@ -5,8 +5,9 @@ from bs4 import BeautifulSoup
 
 url = raw_input('Enter - ')
 html = urllib.urlopen(url).read()  ### this is like a file read opeeration and html variable will contain a string
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, "html.parser")  ## to parse the above string as html data
 # Retrieve all of the anchor tags 
-tags = soup('a')
+tags = soup('a')  ### This will retrieve all the <a  .... /a> from the html
+print tags  ## This will print the list with all the anchor tags , below we are going to fet the href
 for tag in tags:
 	print tag.get('href', None)
